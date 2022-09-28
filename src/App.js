761825d423d669c954c2d59aedc356db182cfe4d
yingyingyw <yingw239@gmail.com>
2022-09-28 21:36:50 +1000

    made Table Number center

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
 import "./App.css";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 import { useStateWithCallback } from "./useStateWithCallback";
-import { useEffect } from 'react'
-import data from './data.json';
+import { useEffect } from "react";
+import data from "./data.json";
 
 const useStyles = makeStyles((themes) => ({
   root: {
     "& .MuiFilledInput-root": {
-      backgroundColor: "white"
+      backgroundColor: "white",
     },
     "& .MuiFilledInput-root:hover": {
       backgroundColor: "white",
       // Reset on touch devices, it doesn't add specificity
       "@media (hover: none)": {
-        backgroundColor: "white"
-      }
+        backgroundColor: "white",
+      },
     },
     "& .MuiFilledInput-root.Mui-focused": {
-      backgroundColor: "white"
+      backgroundColor: "white",
     },
-  }
+  },
 }));
-
 
 function App() {
   const classes = useStyles();
-  const [tableNumber,setTableNumber] = useStateWithCallback("0");
-  const [wrongName,setWrongName] = useStateWithCallback(false);
+  const [tableNumber, setTableNumber] = useStateWithCallback("0");
+  const [wrongName, setWrongName] = useStateWithCallback(false);
 
   const [name, setName] = useStateWithCallback("");
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(name.toLowerCase().replace(/\s/g,''));
-    if(name.length===0){
+    console.log(name.toLowerCase().replace(/\s/g, ""));
+    if (name.length === 0) {
       setWrongName(true);
       return;
     }
@@ -46,10 +45,15 @@ function App() {
     // ..code to submit form to backend here...
     setTableNumber("-1");
     setWrongName(false);
-    
+
     data.forEach((singleHeroObject, index) => {
       Object.values(singleHeroObject).every((onlyValues, valIndex) => {
-        if (onlyValues.toLowerCase().replace(/\s/g,'').includes(name.toLowerCase().replace(/\s/g,''))) {
+        if (
+          onlyValues
+            .toLowerCase()
+            .replace(/\s/g, "")
+            .includes(name.toLowerCase().replace(/\s/g, ""))
+        ) {
           setTableNumber(singleHeroObject.tableNumber);
           return;
         }
@@ -59,11 +63,10 @@ function App() {
   }
 
   useEffect(() => {
-    if(tableNumber==="-1"){
+    if (tableNumber === "-1") {
       setWrongName(true);
     }
-  })
-  
+  });
 
   return (
     <div>
@@ -82,30 +85,35 @@ function App() {
 
         <Grid className="nameAndTable">
           <form className="nameInput" onSubmit={handleSubmit}>
-            <TextField error={wrongName} variant="filled" className={classes.root} id="name" 
-            label={wrongName?"Invalid Name":"Input Name"} type="name" style = {{width: 250}} value={name} onInput={e=>setName(e.target.value)}/>
+            <TextField
+              error={wrongName}
+              variant="filled"
+              className={classes.root}
+              id="name"
+              label={wrongName ? "Invalid Name" : "Input Name"}
+              type="name"
+              style={{ width: 250 }}
+              value={name}
+              onInput={(e) => setName(e.target.value)}
+            />
             <Typography className={classes.divider} />
             <Grid item>
-              <Button
-                  type="submit"
-                  variant="contained"
-                  sx={{ mt: 2, mb: 2 }}
-                >
-                  Submit
+              <Button type="submit" variant="contained" sx={{ mt: 2, mb: 2 }}>
+                Submit
               </Button>
             </Grid>
           </form>
         </Grid>
-        {!(tableNumber==="-1") && !(tableNumber==="0") ?
-        <div className="tableNumberContainer"> 
-          <div className="tableNumberText">
-            <div className="tableNumberHeader">
-              Table Number
+        {!(tableNumber === "-1") && !(tableNumber === "0") ? (
+          <div className="tableNumberContainer">
+            <div className="tableNumberText">
+              <div className="tableNumberHeader">Table Number</div>
+              <div className="tableNumber">{tableNumber}</div>
             </div>
-            <div className="tableNumber">{tableNumber}</div>
           </div>
-        </div>
-        : <></>}  
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
